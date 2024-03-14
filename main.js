9acf7fa9202a272974d9b40b083c063db01066eb
pedroDev <playstarpedro@gmail.com>
2024-03-14 13:02:00 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplicar(2, 3));
+function saudar(nome) {
+    return `Olá ${nome}!`;
+}
+console.log(saudar('Pedro'));
---
+++ main.ts
@@ -0,0 +1,11 @@
+function multiplicar(num1: number, num2: number) {
+    return num1 * num2
+}
+
+console.log(multiplicar(2, 3))
+
+function saudar(nome: string) {
+    return `Olá ${nome}!`
+}
+
+console.log(saudar('Pedro'))
